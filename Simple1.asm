8029cf50e8170785dc6919ae83f3151fa4288725
hx1013 <hx1013@skbl-422-009.ph.ic.ac.uk>
2019-01-14 10:36:13 +0000

    something

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,19 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw	0xff
+	movwf	TRISD, ACCESS
 	movlw 	0x0
-	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movwf	TRISC, ACCESS	    ; Port C all outputs 
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
+	
+;	movlw 	0x63
+	movf	PORTD, W, ACCESS
+;	movlw	0xff
+	
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
