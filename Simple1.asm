3323669424b11ee710c8a668837e378548a6e524
hx1013 <hx1013@skbl-422-009.ph.ic.ac.uk>
2019-01-14 16:10:57 +0000

    Wrote main program

--- Simple1.asm
+++ Simple1.asm
@@ -7,8 +7,8 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 	; ******* Programme FLASH read Setup Code ****  
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
+;setup	bcf	EECON1, CFGS	; point to Flash program memory  
+;	bsf	EECON1, EEPGD 	; access Flash program memory
 	
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
@@ -16,22 +16,28 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	
 	goto	start
 	; ******* My data and where to put it in RAM *
-myTable data	"This is just some data"
-	constant 	myArray=0x400	; Address in RAM for data
+myTable db	0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80
 	constant 	counter=0x10	; Address of counter variable
 	; ******* Main programme *********************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
+
+	
+	
+;	CHUNK LOADS 1 BYTE FROM MYTABLE
+;	CREATES TBLPTR THAT POINTS TO MYTABLE
+start	movlw	upper(myTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
 	movwf	TBLPTRH		; load high byte to TBLPTRH
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-	decfsz	counter		; count down to zero
+	
+	movlw	.8		; 8 bytes to read - counter
+	movwf	counter		; Initialise counter
+	
+loop	tblrd*+			; move one byte from PM to TABLAT, increment TBLPTR
+	movfw	TABLAT		; Move TABLAT (TBLPTR value) to W
+	movwf	PORTC		; Move W to PORTC (TBLPTR value)
+	decfsz	counter		; Decrement counter, skip if zero
 	bra	loop		; keep going until finished
 	
 	goto	0
